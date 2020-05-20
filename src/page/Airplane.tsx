88c0e7906a38de794b7a4c27d16a0ca354d0e752
tamo3 <tanabe@pdx.edu>
2020-05-20 01:42:22 -0700

    - To draw positions of airplanes in map using historical data. - Use random colors for airplane positions.

--- src/page/Airplane.tsx
+++ src/page/Airplane.tsx
@@ -1,14 +1,9 @@
 import React, { FC } from "react";
 import {
   Entity,
-  Model,
-  ModelGraphics,
-  Billboard,
-  CameraFlyTo,
   PointGraphics,
-  BillboardGraphics,
-  BoxGraphics,
-  EllipseGraphics,
+  // BoxGraphics,
+  // EllipseGraphics,
 } from "resium";
 import { Cartesian3, Transforms, Color } from "cesium";
 
@@ -16,8 +11,15 @@ type AirplaneProps = {
   lng: number;
   lat: number;
   high: number;
-  color: Color;
+  color?: Color;
 };
+
+function randomColor(): Color {
+  const r = Math.random();
+  const g = Math.random();
+  const b = Math.random();
+  return new Color(r, g, b);
+}
 
 export const Airplane: FC<AirplaneProps> = ({ lng, lat, high, color }) => {
   // const position = Cartesian3.fromDegrees(-122.595172, 45.5895, 100);
@@ -26,7 +28,7 @@ export const Airplane: FC<AirplaneProps> = ({ lng, lat, high, color }) => {
     <div>
       <Entity position={position}>
         {/* <EllipseGraphics semiMajorAxis={100} semiMinorAxis={100} height={500} material={Color.CYAN} /> */}
-        <PointGraphics color={color} pixelSize={10} />
+        <PointGraphics color={color || randomColor()} pixelSize={10} />
       </Entity>
     </div>
   );
--- src/page/History.tsx
+++ src/page/History.tsx
@@ -1,34 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 // import ReactDOM from 'react-dom';
 import {
   Viewer,
   Entity /*PointGraphics*/,
+  CameraFlyTo,
   Clock,
   Globe,
-  Model,
-  ModelGraphics,
-  BillboardCollection,
-  Billboard,
-  CameraFlyTo,
-  PointGraphics,
-  BillboardGraphics,
-  BoxGraphics,
-  EllipseGraphics,
+  // Model,
+  // ModelGraphics,
+  // Billboard,
+  // PointGraphics,
+  // BoxGraphics,
+  // EllipseGraphics,
 } from "resium";
 import { Cartesian3, Transforms, Color } from "cesium";
 import Airplane from "./Airplane";
 
 import "./Map.css";
 
-const position = Cartesian3.fromDegrees(-122.595172, 45.5895, 100); // (-122.595172, 45.5895, 10)}  (PDX: long, lat)
-const cameraDest = Cartesian3.fromDegrees(-122.595172, 45.5895, 210000);
+type Pos2D = {
+  lat: number;
+  lng: number;
+};
+// const origPos: Pos2D = {lat:33.974, lng:-118.322}; // LA
+const origPos: Pos2D = { lat: 45.5895, lng: -122.595172 }; // PDX.
+const position = Cartesian3.fromDegrees(origPos.lng, origPos.lat, 100);
+const cameraDest = Cartesian3.fromDegrees(origPos.lng, origPos.lat, 210000);
 
 function HistoryPage() {
-  const [airplanes, updateAirplanes] = React.useState([]);
+  const urlBase: string = "http://localhost:5000/api?file=2016-07-01-1400Z.json&lng=_LNG_&lat=_LAT_&range=1000000";
 
-  // const SetResult: FC<{ s: string }> = ({ s }) => {
-  //   return <p>{s}</p>;
-  // };
+  const [airplaneData, setAirplaneData] = React.useState<any>([]);
+  // const [url, setUrl] = React.useState(urlBase);
+  const [posTime, setPosTime] = React.useState<Pos2D>({
+    lng: origPos.lng,
+    lat: origPos.lat,
+  });
 
   let counter: number = 0;
   function onTick() {
@@ -40,36 +47,39 @@ function HistoryPage() {
     }
   }
 
-  // LA: -118.322, 33.974
-  const urlBase: string = "http://localhost:5000/api?file=2016-07-01-1400Z.json&lng=-118.322&lat=33.974&range=10000";
-  let url: string;
-
-  function fetchData() {
+  useEffect(() => {
+    // Replace longitude & latitude in URL query string.
+    const url = urlBase.replace("_LNG_", posTime.lng.toString()).replace("_LAT_", posTime.lat.toString());
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        // console.log("this is json");
         console.log(data);
+        setAirplaneData(data); // Set the received data array.
       })
       .catch((err) => {
         console.log("Error!");
         console.log(err);
       });
-  }
+  }, [posTime]);
 
-  url = urlBase;
+  const pos: Pos2D = origPos;
   return (
     <div className='cesiumContainer'>
-      <button onClick={fetchData}>Click me</button>
+      <button onClick={() => setPosTime(pos)}>Click me</button>
       <Viewer>
         <Entity position={position}>
           <CameraFlyTo destination={cameraDest} duration={1} />
+          <div>
+            {airplaneData.map((x: any) => {
+              return <Airplane key={x.Id} lng={x.Cos[1]} lat={x.Cos[0]} high={x.Cos[3]} />;
+            })}
+          </div>
           <Airplane lng={-122.595172} lat={45.5895} high={100} color={Color.CYAN} />
         </Entity>
         <Globe enableLighting />
-        <Clock onTick={onTick} />
+        {/* <Clock onTick={onTick} /> */}
       </Viewer>
     </div>
   );
