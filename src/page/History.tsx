15009305e4fb30c9ee5523b711bcc9bab6ea8ddd
tamo3 <tanabe@pdx.edu>
2020-05-21 01:30:19 -0700

    - To move airplanes based on internal time.

--- src/page/History.tsx
+++ src/page/History.tsx
@@ -6,6 +6,7 @@ import {
   CameraFlyTo,
   Clock,
   Globe,
+  Scene,
   // Model,
   // ModelGraphics,
   // Billboard,
@@ -27,29 +28,41 @@ const origPos: Pos2D = { lat: 45.5895, lng: -122.595172 }; // PDX.
 const position = Cartesian3.fromDegrees(origPos.lng, origPos.lat, 100);
 const cameraDest = Cartesian3.fromDegrees(origPos.lng, origPos.lat, 210000);
 
+let counter: number = 0;
+
 function HistoryPage() {
-  const urlBase: string = "http://localhost:5000/api?file=2016-07-01-1400Z.json&lng=_LNG_&lat=_LAT_&range=1000000";
+  const urlBase: string = "http://localhost:5000/api?file=2016-07-01-_TIME_Z.json&lng=_LNG_&lat=_LAT_&range=1000000";
 
   const [airplaneData, setAirplaneData] = React.useState<any>([]);
-  // const [url, setUrl] = React.useState(urlBase);
   const [posTime, setPosTime] = React.useState<Pos2D>({
     lng: origPos.lng,
     lat: origPos.lat,
   });
+  const [curTime, setCurTime] = React.useState<number>(0);
 
-  let counter: number = 0;
+  function update() {
+    setCurTime(curTime + 1);
+  }
+
   function onTick() {
-    if (counter++ % 64 === 0) {
-      // UPdate about every 1 second.
-      // const t = Math.floor(counter / 64);
-      // SetResult({counter.toString()});
-      console.log(`QQQ`);
+    if (counter++ % 128 === 0) {
+      // UPdate about every 2 second.
+      // setCurTime(curTime + 1);
+      update();
+      console.log(`QQQ ${counter}`);
     }
   }
 
   useEffect(() => {
     // Replace longitude & latitude in URL query string.
-    const url = urlBase.replace("_LNG_", posTime.lng.toString()).replace("_LAT_", posTime.lat.toString());
+    const tm = curTime % 2400;
+    const min = Math.floor(tm % 60);
+    const hour = Math.floor(tm / 60);
+    const timeStr = hour.toString().padStart(2, "0") + min.toString().padStart(2, "0");
+    const url = urlBase
+      .replace("_LNG_", posTime.lng.toString())
+      .replace("_LAT_", posTime.lat.toString())
+      .replace("_TIME_", timeStr);
     fetch(url)
       .then((response) => {
         return response.json();
@@ -62,24 +75,26 @@ function HistoryPage() {
         console.log("Error!");
         console.log(err);
       });
-  }, [posTime]);
+  }, [posTime, curTime]);
 
   const pos: Pos2D = origPos;
+  console.log(`curTime is ${curTime}`);
   return (
     <div className='cesiumContainer'>
       <button onClick={() => setPosTime(pos)}>Click me</button>
       <Viewer>
-        <Entity position={position}>
-          <CameraFlyTo destination={cameraDest} duration={1} />
+        {curTime === 0 && <CameraFlyTo destination={cameraDest} duration={3} />}
+        {/* <Scene debugShowFramesPerSecond={true} /> */}
+        <Entity>
           <div>
+            <Clock onTick={onTick} />
             {airplaneData.map((x: any) => {
-              return <Airplane key={x.Id} lng={x.Cos[1]} lat={x.Cos[0]} high={x.Cos[3]} />;
+              return <Airplane key={x.Id} lng={x.Cos[1]} lat={x.Cos[0]} high={x.Cos[3]} color={Color.RED} />;
             })}
           </div>
-          <Airplane lng={-122.595172} lat={45.5895} high={100} color={Color.CYAN} />
+          {/* <Airplane lng={-122.595172} lat={45.5895} high={100} color={Color.CYAN} /> */}
         </Entity>
         <Globe enableLighting />
-        {/* <Clock onTick={onTick} /> */}
       </Viewer>
     </div>
   );
